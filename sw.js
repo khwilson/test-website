3a6a6b7ca87604d6e2ab8e3ee51a75f0ad0d8c04
khwilson <khwilson@gmail.com>
2021-03-03 19:17:41 +0000

    Deploying to gh-pages from  @ 6b2b9e28f49874185ea7a88e25918a75e5753dfc 🚀

--- sw.js
+++ sw.js
@@ -45,11 +45,11 @@ self.__precacheManifest = [
     "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-f1aaf66d03f12bc670e6.js"
   },
   {
-    "url": "app-82b4adfa8b95fee7a7a6.js"
+    "url": "app-f4740f38440e11e78d2a.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "65cdd1f5ed2cd02b8f5bb7d51c0f1aa7"
+    "revision": "7878f6bd5276e54d7828f58bc7af948a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fd4fb51a6fac1c18bdde.js"
@@ -155,7 +155,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-82b4adfa8b95fee7a7a6.js`))) {
+  if (!resources || !(await caches.match(`/app-f4740f38440e11e78d2a.js`))) {
     return await fetch(event.request)
   }
 
